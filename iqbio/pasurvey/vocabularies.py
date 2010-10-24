2cfbc811676313a7c2fdbc34c7e65639db6186ca
Kurt Bendl <kurt@tool.net>
2010-10-24 15:57:30 -0600

    Fix-up vocabs so they don't break. Man, I'm glad I'm the only one working on this at the moment.

--- iqbio/pasurvey/vocabularies.py
+++ iqbio/pasurvey/vocabularies.py
@@ -1,99 +1,92 @@
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from iqbio.pasurvey import _
 
-degreeprograms_vocab = ([
-        SimpleTerm(value=u'AppliedMath', title=_(u'Applied Mathematics (Arts & Sciences)')),
-        SimpleTerm(value=u'Biochemistry', title=_(u'Biochemistry (Arts & Sciences)')),
-        SimpleTerm(value=u'ChemBioEngineering', title=_(u'Chemical and Biological Engineering (listed as Chemical Engineering in College of Engineering & Applied Science)')),
-        SimpleTerm(value=u'ComputerScience', title=_(u'Computer Science (Engineering & Applied Science)')),
-        SimpleTerm(value=u'Ecology', title=_(u'Ecology and Evolutionary Biology (Arts & Sciences)')),
-        SimpleTerm(value=u'Mechanical', title=_(u'Mechanical Engineering (Engineering & Applies Science)')),
-        SimpleTerm(value=u'Molecular', title=_(u'Molecular, Cellular and Developmental Biology (Arts & Sciences)')),
-        SimpleTerm(value=u'ChemicalPhysics', title=_(u'Chemical Physics (Arts & Sciences).')),
-        ])
+degreeprograms_vocab = SimpleVocabulary.fromItems((
+    ('AppliedMath', 'Applied Mathematics (Arts & Sciences)'),
+    ('Biochemistry', 'Biochemistry (Arts & Sciences)'),
+    ('ChemBioEngineering', 'Chemical and Biological Engineering (listed as Chemical Engineering in College of Engineering & Applied Science)'),
+    ('ComputerScience', 'Computer Science (Engineering & Applied Science)'),
+    ('Ecology', 'Ecology and Evolutionary Biology (Arts & Sciences)'),
+    ('Mechanical', 'Mechanical Engineering (Engineering & Applies Science)'),
+    ('Molecular', 'Molecular, Cellular and Developmental Biology (Arts & Sciences)'),
+    ('ChemicalPhysics', 'Chemical Physics (Arts & Sciences).'),
+    ))
 
 #----------------------------------------------------------------------
-def biochem_research_interests_vocab(context):
-    ''' '''
-    vocab = ('Bio-Organic and Bio-Inorganic',
-        'Informatics and Proteomics',
-        'Nucleic Acids',
-        'Cell Signaling',
-        'Membranes',
-        'Proteins and Enzymology',
-        'Chemical Biology/Genetics',
-        'Molecular Biophysics',
-        'Structural Biology',
-        'Chemical Physics',
-        'Biophysics',
-        'Atmospheric/Astrochemistry',
-        'Kinetics/Thermodynamics',
-        'Reaction Dynamics',
-        'Surface Chemistry',
-        'Photochemistry',
-        'Physical Organic Chemistry',
-        'Theoretical Chemistry',
-        'Nanotechnology/Materials',
-        'Physical Inorganic Chemistry',
-        )
-
-    return SimpleVocabulary.fromValues(vocab)
+biochem_research_interests_vocab = SimpleVocabulary.fromValues((
+    ('Bio-Organic and Bio-Inorganic'),
+    ('Informatics and Proteomics'),
+    ('Nucleic Acids'),
+    ('Cell Signaling'),
+    ('Membranes'),
+    ('Proteins and Enzymology'),
+    ('Chemical Biology/Genetics'),
+    ('Molecular Biophysics'),
+    ('Structural Biology'),
+    ('Chemical Physics'),
+    ('Biophysics'),
+    ('Atmospheric/Astrochemistry'),
+    ('Kinetics/Thermodynamics'),
+    ('Reaction Dynamics'),
+    ('Surface Chemistry'),
+    ('Photochemistry'),
+    ('Physical Organic Chemistry'),
+    ('Theoretical Chemistry'),
+    ('Nanotechnology/Materials'),
+    ('Physical Inorganic Chemistry'),
+    ))
 
 
-#
-def facultyofinterest_vocab(context):
-    ''' '''
-    vocab = ('Thomas R. Cech',
-        'Kristi S. Anseth',
-        'Meredith D. Betterton',
-        'David M. Bortz',
-        'Stephanie J. Bryant',
-        'Aaron Clauset',
-        'Robin Dowell',
-        'Virginia L. Ferguson',
-        'Matthew A. Glaser',
-        'Debra S. Goldberg',
-        'Rob Knight',
-        'Leslie A. Leinwand',
-        'Manuel E. Lladser',
-        'Brett A. Melbourne',
-        'David. J. Nesbitt',
-        'Arthur Pardi',
-        'Amy. E. Palmer',
-        'Thomas T. Perkins',
-        'Hang (Hubert) Yin',
-        )
-    return SimpleVocabulary.fromValues(vocab)
+facultyofinterest_vocab = SimpleVocabulary.fromItems((
+    ('Cech', 'Thomas R. Cech'),
+    ('Anseth', 'Kristi S. Anseth'),
+    ('Betterton', 'Meredith D. Betterton'),
+    ('Bortz', 'David M. Bortz'),
+    ('Bryant', 'Stephanie J. Bryant'),
+    ('Clauset', 'Aaron Clauset'),
+    ('Dowell', 'Robin Dowell'),
+    ('Ferguson', 'Virginia L. Ferguson'),
+    ('Glaser', 'Matthew A. Glaser'),
+    ('Goldberg', 'Debra S. Goldberg'),
+    ('Knight', 'Rob Knight'),
+    ('Leinwand', 'Leslie A. Leinwand'),
+    ('Lladser', 'Manuel E. Lladser'),
+    ('Melbourne', 'Brett A. Melbourne'),
+    ('Nesbitt', 'David. J. Nesbitt'),
+    ('Pardi', 'Arthur Pardi'),
+    ('Palmer', 'Amy. E. Palmer'),
+    ('Perkins', 'Thomas T. Perkins'),
+    ('Yin', 'Hang (Hubert) Yin'),
+    ))
 
 
-def bio_chem_vocab(context):
-    vocab = (
-        'Bio-Organic and Bio-Inorganic',
-        'Informatics and Proteomics',
-        'Nucleic Acids',
-        'Cell Signaling',
-        'Membranes',
-        'Proteins and Enzymology',
-        'Chemical Biology/Genetics',
-        'Molecular Biophysics',
-        'Structural Biology',
-        'Chemical Physics',
-        'Biophysics',
-        'Atmospheric/Astrochemistry',
-        'Kinetics/Thermodynamics',
-        'Reaction Dynamics',
-        'Surface Chemistry',
-        'Photochemistry',
-        'Physical Organic Chemistry',
-        'Theoretical Chemistry',
-        'Nanotechnology/Materials',
-        'Physical Inorganic Chemistry',
-    )
-    return SimpleVocabulary.fromValues(vocab)
+
+bio_chem_vocab = SimpleVocabulary.fromValues((
+    ('Bio-Organic and Bio-Inorganic'),
+    ('Informatics and Proteomics'),
+    ('Nucleic Acids'),
+    ('Cell Signaling'),
+    ('Membranes'),
+    ('Proteins and Enzymology'),
+    ('Chemical Biology/Genetics'),
+    ('Molecular Biophysics'),
+    ('Structural Biology'),
+    ('Chemical Physics'),
+    ('Biophysics'),
+    ('Atmospheric/Astrochemistry'),
+    ('Kinetics/Thermodynamics'),
+    ('Reaction Dynamics'),
+    ('Surface Chemistry'),
+    ('Photochemistry'),
+    ('Physical Organic Chemistry'),
+    ('Theoretical Chemistry'),
+    ('Nanotechnology/Materials'),
+    ('Physical Inorganic Chemistry'),
+    ))
 
 
-def chem_bio_vocab(context):
-    vocab = ('Advanced Ceramic Materials',
+chem_bio_vocab = SimpleVocabulary.fromValues((
+        'Advanced Ceramic Materials',
         'Applied Math and Computers',
         'Biomedical Engineering',
         'Biotechnology',
@@ -111,51 +104,43 @@ def chem_bio_vocab(context):
         'Separations',
         'Thermodynamics',
         'Transport Processes',
-    )
-    return SimpleVocabulary.fromValues(vocab)
+    ))
 
 
-def comp_sci_vocab(context):
-    vocab = (
-        'Bio and Medical Informatics',
-        'Computational Modeling of Human Cognition',
-        'Computational Science',
-        'Computer Architecture',
-        'Database Systems',
-        'Distributed and Network Computing',
-        'Human-Centered Computing',
-        'Machine Learning',
-        'Machine Vision',
-        'Numerical and Scientific Computation',
-        'Operating Systems',
-        'Programming Languages',
-        'Robotics',
-        'Security',
-        'Software Engineering',
-        'Speech and Language Processing',
-        'Theory',
-    )
-    return SimpleVocabulary.fromValues(vocab)
+comp_sci_vocab = SimpleVocabulary.fromValues((
+    ('Bio and Medical Informatics'),
+    ('Computational Modeling of Human Cognition'),
+    ('Computational Science'),
+    ('Computer Architecture'),
+    ('Database Systems'),
+    ('Distributed and Network Computing'),
+    ('Human-Centered Computing'),
+    ('Machine Learning'),
+    ('Machine Vision'),
+    ('Numerical and Scientific Computation'),
+    ('Operating Systems'),
+    ('Programming Languages'),
+    ('Robotics'),
+    ('Security'),
+    ('Software Engineering'),
+    ('Speech and Language Processing'),
+    ('Theory'),
+    ))
+
+comp_sci_financial_aid_vocab = SimpleVocabulary.fromItems((
+    ('MustHave', u'I will not accept admission without financial aid.'),
+    ('WillConsider', u'I have my own financial support, but would like to be considered for financial aid.'),
+    ('DoNotWant', u'I do not wish to be considered for financial aid.'),
+    ))
+
+yes_no_vocab = SimpleVocabulary.fromItems((
+    ('', u'-----'),
+    ('Yes', u'Yes'),
+    ('No', u'No'),
+    ))
 
 
-def comp_sci_financial_aid_vocab(context):
-    return ([
-    SimpleTerm(value=u'MustHave', title=_(u'I will not accept admission without financial aid.')),
-    SimpleTerm(value=u'WillConsider', title=_(u'I have my own financial support, but would like to be considered for financial aid.')),
-    SimpleTerm(value=u'DoNotWant', title=_(u'I do not wish to be considered for financial aid.')),
-    ])
-
-
-def yes_no_vocab (context):
-    return ([
-    SimpleTerm(value=u'', title=_(u'-----')),
-    SimpleTerm(value=u'Yes', title=_(u'Yes')),
-    SimpleTerm(value=u'No', title=_(u'No')),
-    ])
-
-
-def exp_or_theoretical_vocab (context):
-    return ([
-    SimpleTerm(value=u'Experimental', title=_(u'Experimental')),
-    SimpleTerm(value=u'Theoretical', title=_(u'Theoretical')),
-    ])
+exp_or_theoretical_vocab = SimpleVocabulary.fromItems((
+    ('Experimental', u'Experimental'),
+    ('Theoretical', u'Theoretical'),
+    ))
