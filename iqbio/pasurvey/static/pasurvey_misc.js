38ffbd2f7cd37042d9efe906c0cb8d56720d4090
Kurt Bendl <kurt@tool.net>
2010-11-07 18:54:08 -0700

    Tweak the conditional fields' required fields (not all are required.)

--- iqbio/pasurvey/static/pasurvey_misc.js
+++ iqbio/pasurvey/static/pasurvey_misc.js
@@ -1,14 +1,18 @@
 jq(document).ready(function() {
-    // Tried doing something cool. Gave up.
-    var fsList = ['#fieldset-0 label.horizontal',
-        '#fieldset-1 label.horizontal',
-        '#fieldset-2 label.horizontal',
-        '#fieldset-3 label.horizontal',
-        '#fieldset-4 label.horizontal'];
 
     jq('#fieldset-0 label.horizontal').append(" <span class='req'>(required)</span>");
     jq('#fieldset-1 label.horizontal').append(" <span class='req'>(required)</span>");
     jq('#fieldset-2 label.horizontal').append(" <span class='req'>(required)</span>");
-    jq('#fieldset-3 label.horizontal').append(" <span class='req'>(required)</span>");
+
+    // fieldset-3 is eco
+    //jq('#fieldset-3 label.horizontal').append(" <span class='req'>(required)</span>");
+    jq('#formfield-form-widgets-ecofinancialaid label.horizontal').append(" <span class='req'>(required)</span>");
+    jq('#formfield-form-widgets-ecoundergradgpa label.horizontal').append(" <span class='req'>(required)</span>");
+    jq('#formfield-form-widgets-ecoundergradgpabio label.horizontal').append(" <span class='req'>(required)</span>");
+    jq('#formfield-form-widgets-ecocoursebio label.horizontal').append(" <span class='req'>(required)</span>");
+    jq('#formfield-form-widgets-ecoresearchinterests label.horizontal').append(" <span class='req'>(required)</span>");
+
+    // just need to supress one under chemical physics - fieldset-4
     jq('#fieldset-4 label.horizontal').append(" <span class='req'>(required)</span>");
+    jq("div#formfield-form-widgets-chemphgre span.req").remove();
 });
